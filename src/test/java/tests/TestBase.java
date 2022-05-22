c07a37ad4406b97058289925eb68b0273e2e7c97
Osama-Sayed <os1224@fayoum.edu.eg>
2022-05-22 10:55:39 +0200

    Adding headless

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,10 +1,15 @@
 package tests;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utilities.Helper;
@@ -29,6 +34,24 @@ public  void startDriver(@Optional("chrome") String browserName){
     else if(browserName.equalsIgnoreCase("ie")){
        System.setProperty("webdriver.ie.driver",System.getProperty("user.dir")+"/drivers/IEDriverServer.exe");
         driver = new InternetExplorerDriver();
+    }
+    //Headless Browser
+    else if(browserName.equalsIgnoreCase("headless")){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,System.getProperty("user.dir")+"/drivers/phantomjs.exe");
+        String[] phantomJSArgs ={"--web-security=no","--ignore-ssl-errors=yes"};
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,phantomJSArgs);
+        driver = new PhantomJSDriver(caps);
+    }
+    else if(browserName.equalsIgnoreCase("chrome-headless")){
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--window-site=1920,1080");
+        driver = new ChromeDriver(options);
+
+
     }
 
     driver.manage().window().maximize();
